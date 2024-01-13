56991be3115bcfcee629176613daaade68cb4def
John Kim <mkcokr2176@gmail.com>
2024-01-13 11:34:59 -0800

    Update requirements.txt file for deployment

--- api/prediction_script.py
+++ api/prediction_script.py
@@ -1,10 +1,6 @@
-import numpy as np
 import pandas as pd
 import xgboost as xgb
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
 import joblib
 
 def load_models():
